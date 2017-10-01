c12071a9b56feb5436b160779cb435303f59ec90
db06370 <db06370@CEIT2551204X040>
2017-10-01 17:18:42 -0400

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.lvProducts = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(12, 12);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(158, 23);
+            this.btn_Connect.TabIndex = 0;
+            this.btn_Connect.Text = "Connect to database!";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // lvProducts
+            // 
+            this.lvProducts.GridLines = true;
+            this.lvProducts.Location = new System.Drawing.Point(12, 41);
+            this.lvProducts.Name = "lvProducts";
+            this.lvProducts.Size = new System.Drawing.Size(520, 380);
+            this.lvProducts.TabIndex = 1;
+            this.lvProducts.UseCompatibleStateImageBehavior = false;
+            this.lvProducts.View = System.Windows.Forms.View.Details;
+            this.lvProducts.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 433);
+            this.Controls.Add(this.lvProducts);
+            this.Controls.Add(this.btn_Connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.ListView lvProducts;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=CEIT2551204X040\\LOCAL;Initial Catalog=MyGuitarShop;Integrated Security=True";
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open !");
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Products", cnn);
+                // Create new SqlDataReader object and read data from the command.
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    DataTable tableSchema = reader.GetSchemaTable();
+                    string[] columNames = new string[tableSchema.Rows.Count];
+
+                    for(int i = 0; i < columNames.Length; i++)
+                    {
+                        columNames[i] = tableSchema.Rows[i]["ColumnName"].ToString();
+                    }
+
+                    foreach(string column in columNames)
+                    {
+                        lvProducts.Columns.Add(column, -2, HorizontalAlignment.Left);
+                    }
+
+                    // while there is another record present
+                    while (reader.Read())
+                    {
+                        string[] rowData = new string[columNames.Length];
+                        for(int i = 0; i < rowData.Length; i++)
+                        {
+                            rowData[i] = reader[i].ToString();
+                        }
+
+                        lvProducts.Items.Add(new ListViewItem(rowData));
+                    }
+
+                    lvProducts.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                }
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
