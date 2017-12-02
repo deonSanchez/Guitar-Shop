515ca825e07ceb48c4b3ab741650b60e27e7e6f6
Daniel Burer <dwburer@gmail.com>
2017-12-02 12:29:49 -0500

    add missing files

--- WindowsFormsApplication1/RepairItemSelection.Designer.cs
+++ WindowsFormsApplication1/RepairItemSelection.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.lbl_Item = new System.Windows.Forms.Label();
             this.cmb_Item = new System.Windows.Forms.ComboBox();
+            this.lbl_repairType = new System.Windows.Forms.Label();
+            this.txt_repairType = new System.Windows.Forms.TextBox();
+            this.lbl_labor = new System.Windows.Forms.Label();
+            this.updn_labor = new System.Windows.Forms.NumericUpDown();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.updn_labor)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_Item
@@ -45,19 +52,91 @@
             // 
             this.cmb_Item.FormattingEnabled = true;
             this.cmb_Item.Location = new System.Drawing.Point(15, 25);
+            this.cmb_Item.Margin = new System.Windows.Forms.Padding(3, 3, 3, 6);
             this.cmb_Item.Name = "cmb_Item";
             this.cmb_Item.Size = new System.Drawing.Size(257, 21);
             this.cmb_Item.TabIndex = 2;
+            // 
+            // lbl_repairType
+            // 
+            this.lbl_repairType.AutoSize = true;
+            this.lbl_repairType.Location = new System.Drawing.Point(12, 52);
+            this.lbl_repairType.Name = "lbl_repairType";
+            this.lbl_repairType.Size = new System.Drawing.Size(65, 13);
+            this.lbl_repairType.TabIndex = 3;
+            this.lbl_repairType.Text = "Repair Type";
+            // 
+            // txt_repairType
+            // 
+            this.txt_repairType.Location = new System.Drawing.Point(15, 68);
+            this.txt_repairType.Margin = new System.Windows.Forms.Padding(3, 3, 3, 6);
+            this.txt_repairType.Name = "txt_repairType";
+            this.txt_repairType.Size = new System.Drawing.Size(257, 20);
+            this.txt_repairType.TabIndex = 4;
+            // 
+            // lbl_labor
+            // 
+            this.lbl_labor.AutoSize = true;
+            this.lbl_labor.Location = new System.Drawing.Point(12, 94);
+            this.lbl_labor.Name = "lbl_labor";
+            this.lbl_labor.Size = new System.Drawing.Size(58, 13);
+            this.lbl_labor.TabIndex = 5;
+            this.lbl_labor.Text = "Labor Cost";
+            // 
+            // updn_labor
+            // 
+            this.updn_labor.DecimalPlaces = 2;
+            this.updn_labor.Location = new System.Drawing.Point(15, 110);
+            this.updn_labor.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.updn_labor.Name = "updn_labor";
+            this.updn_labor.Size = new System.Drawing.Size(257, 20);
+            this.updn_labor.TabIndex = 6;
+            this.updn_labor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.updn_labor.ThousandsSeparator = true;
+            this.updn_labor.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(197, 166);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 8;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Enabled = false;
+            this.btn_add.Location = new System.Drawing.Point(116, 166);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 23);
+            this.btn_add.TabIndex = 7;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
             // 
             // RepairItemSelection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 200);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.updn_labor);
+            this.Controls.Add(this.lbl_labor);
+            this.Controls.Add(this.txt_repairType);
+            this.Controls.Add(this.lbl_repairType);
             this.Controls.Add(this.cmb_Item);
             this.Controls.Add(this.lbl_Item);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "RepairItemSelection";
             this.Text = "RepairItemSelection";
+            ((System.ComponentModel.ISupportInitialize)(this.updn_labor)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,5 +145,11 @@
         #endregion
         private System.Windows.Forms.Label lbl_Item;
         private System.Windows.Forms.ComboBox cmb_Item;
+        private System.Windows.Forms.Label lbl_repairType;
+        private System.Windows.Forms.TextBox txt_repairType;
+        private System.Windows.Forms.Label lbl_labor;
+        private System.Windows.Forms.NumericUpDown updn_labor;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_add;
     }
 }
--- WindowsFormsApplication1/RepairItemSelection.cs
+++ WindowsFormsApplication1/RepairItemSelection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,58 @@ namespace GuitarShop
 {
     public partial class RepairItemSelection : Form
     {
-        public RepairItemSelection()
+        RepairForm parent;
+
+        static SqlConnection cnn;
+
+        public RepairItemSelection(RepairForm parent)
         {
             InitializeComponent();
+            this.parent = parent;
+
+            cnn = new SqlConnection(Constants.ConnectionString);
+
+            try
+            {
+                cnn.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error: the connection could not be opened.");
+                Close();
+            }
+        }
+
+        private void loadItems()
+        {
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = cnn;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT ProductID, ProductName, Price FROM Products WHERE ProductType = 'instrument';";
+
+                // Create new SqlDataReader object and read data from the command.
+                try
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<string> customerList = new List<string>();
+
+                        while (reader.Read())
+                        {
+                            string displayString = reader[0].ToString() + " - " + reader[1] + " - " + reader[2];
+                            customerList.Add(displayString);
+                            //orderItemRegistry.Add(displayString, new RepairItem(int.Parse(reader[0].ToString()), decimal.Parse(reader[2].ToString()), reader[1].ToString()));
+                        }
+
+                        //cmb_orderItemSelect.Items.AddRange(customerList.ToArray());
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Could not open Products.");
+                }
+            }
         }
     }
 }
